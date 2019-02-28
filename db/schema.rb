22bfaec788be844258069e06be964c5e475098a7
“Videsh <videsh.patel@gmail.com>
2019-02-28 16:09:05 -0600

    Forgot to commit... Created conference app, created db, crated model, added model instances and added controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_230143) do
+ActiveRecord::Schema.define(version: 2019_02_28_195903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "title"
+    t.text "agenda"
+    t.string "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "speakers", force: :cascade do |t|
     t.string "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first
 
-Speaker.create({first_name: "Stephanie", last_name: "Fountain", email: "sf@actualize.co"})
+Meeting.create({title: "Pass time", agenda: "Waste time on your bosses pay.", time: "March 2nd 2019 2:00 p.m."})
+
+Meeting.create({title: "Meeting about meeting", agenda: "Talk about why we have meetings in the first place.", time: "March 31st 2019 1:00 p.m."})
+
+Meeting.create({title: "Why are we doing this?", agenda: "Talk about why we even work here.", time: "March 5th 2019 4:00 p.m."})
