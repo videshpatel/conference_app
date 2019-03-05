2cdad4ab49d229d5630db27bb190af74720e67d6
“Videsh <videsh.patel@gmail.com>
2019-03-05 16:32:12 -0600

    Something happened, so commiting again.... Added show page.

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -2,7 +2,13 @@ class Api::MeetingsController < ApplicationController
   def index
     @meetings = Meeting.all
     render 'index.json.jbuilder'
+  end
    
+  def show
+    id = params[:id]
+    @meeting = Meeting.find_by(id: id)
+    render 'show.json.jbuilder'
+    
   end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     get '/meetings' => 'meetings#index'
+    get '/meetings/:id' => 'meetings#show'
   end
 end
